f6a7ef2ead8097c1238d2dd0b8095164bbd3b360
Raply Erdani <raflyerdani2@gmail.com>
2025-09-07 17:15:47 +0700

    a

--- src/view/page/favoritePage.js
+++ src/view/page/favoritePage.js
@@ -16,8 +16,22 @@ export const favoritePage = async (root) => {
                     <img src="${s.photoUrl}" alt="${s.name}" width="200">
                     <h3>${s.name}</h3>
                     <p>${s.description}</p>
-                    <button class="delete-fav" data-id="${s.id}">❌ Hapus</button>
                   </div>
+
+                  <div id="card-story" tabindex="0">
+                  <a href="#/stories/${s.id}">
+                    <div id="card-story-img">
+                      <img src="${s.photoUrl}" alt="Nama akun ${s.name} dengan deskripsi ${s.description}"/>
+                    </div>
+                    <div style="display:flex; flex-direction: column; gap: 5px; font-weight: bold">
+                      <h3>Nama: ${s.name}</h3>
+                      <h3>Deskripsi: ${s.description}</h3>
+                      <p>lat: ${s.lat}</p>
+                      <p>lon:${s.lon}</p>
+                    </div>
+                  </a>
+                  <button class="delete-fav" data-id="${s.id}">❌ Hapus</button>
+                </div>
                 `
                 )
                 .join("")
